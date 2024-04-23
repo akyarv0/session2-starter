bb1d3bc438084e7c4639c23fd111a2533c351271
akyarv0 <akyarv0@gmail.com>
2024-04-24 01:00:38 +0300

    login page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
+import React from 'react'
+import Login from './pages/Login'
 
-
-function App() {
-  return (
-   
-
-    <div className="min-h-screen bg-slate-700 text-white text-4xl flex justify-center gap-2 items-center">
+const App = () => {
+  return (<>
+    <Login />
+    {/* <div className="min-h-screen bg-slate-700 text-white text-4xl flex justify-center gap-2 items-center">
        <span className="animate-bounce">🏀</span>
-       <h1 className="animate-pulse">
-        Welcome to Session 1 of the Clarus Store Project FS16!
-       </h1>
+    <h1 className="animate-pulse">
+       Welcome to Session 1 of the Clarus Store Project FS16!
+      </h1>
       <span className="animate-bounce">🏀</span>
-    </div>
-  );
+  </div>*/}
+  </>
+  )
 }
 
-export default App;
+export default App
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div className='loginDiv'>
-    <div className='h-[500px] w-11/12 sm:w-[475px]'></div>
+    <div className="loginDiv">
+      <div className="h-[500px] w-11/12 sm:w-[475px] bg-white rounded-[20px] p-5 flex flex-col items-center">
+      <div className="flex justify-center items-center mt-2 gap-2 " >
+      <span className="w-[6px] h-[39px] bg-yellow-500 "></span><h1 className="text-3xl font-bold uppercase">Clarus Store</h1>  <div className="my-2">
+      <h3>Sign In</h3></div></div>
+      </div>
+    
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
